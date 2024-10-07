b1a5f74fc927fb5d29014e4d24c0e55df384e934
Mehmet Tuna Arıkaya <m.tunaarikaya@gmail.com>
2024-10-07 23:07:56 +0300

    flutter

---
+++ i_am_rich/android/app/src/main/kotlin/com/tuna/i_am_rich/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tuna.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am rich'),
+        backgroundColor:Colors.blueGrey[100] ,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/dia.png'),
+        ),
+      ),
+    ),
+
+  ));
+}//the main func is starting all the flutter apps.
